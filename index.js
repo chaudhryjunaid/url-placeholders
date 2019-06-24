507e92579c0fee7acf771cdebe0cae0d462afeb9
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2019-06-24 14:48:05 +0500

    make es5 compatible

--- index.js
+++ index.js
@@ -2,19 +2,30 @@ const assign = require('lodash.assign');
 const replace = require('lodash.replace');
 const reduce = require('lodash.reduce');
 
-module.exports = function urlPlaceholders({defaults = null, prefix = ':', postfix=''} = {}) {
+module.exports = function urlPlaceholders(_options) {
+  var options = _options || {};
+  // {defaults = null, prefix = ':', postfix=''}
+  if (!options.defaults) {
+    options.defaults = null;
+  }
+  if (!options.prefix) {
+    options.prefix = ':';
+  }
+  if (!options.postfix) {
+    options.postfix = '';
+  }
   return function interpolate(url, _locals) {
-    if (arguments.length === 1 && !defaults) {
+    if (arguments.length === 1 && !options.defaults) {
       return interpolate.bind(null, url);
     }
 
-    const locals = assign({}, defaults, _locals);
-    return reduce(locals, (acc, val, key) => {
+    const locals = assign({}, options.defaults, _locals);
+    return reduce(locals, function(acc, val, key) {
       if (!val) {
         return acc;
       }
-      const toReplace = `${prefix}${key}${postfix}`;
-      const replaceBy = `${val}`;
+      const toReplace = options.prefix + key + options.postfix;
+      const replaceBy = val;
       return replace(acc, toReplace, replaceBy);
     }, url);
   };
